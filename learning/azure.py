d0018843691d7f9dcc99eabcb062836a7abc117b
Arthur <arthur.wilcke@gmail.com>
2016-09-18 02:35:11 -0400

    can now query azure for classifier

---
+++ learning/azure.py
@@ -0,0 +1,43 @@
+import urllib2
+import json 
+from key import api_key
+
+def getClass(anger,fear,disgust,sadness,joy):
+
+    data =  {
+
+            "Inputs": {
+
+                    "input1":
+                    {
+                        "ColumnNames": ["anger", "fear", "disgust", "sadness", "joy", "good"],
+                        "Values": [ [ anger, fear, disgust, sadness, joy, '0']  ]
+                    },        },
+                "GlobalParameters": {
+    }
+        }
+
+    body = str.encode(json.dumps(data))
+
+    url = 'https://ussouthcentral.services.azureml.net/workspaces/e998d9efb75c4bb9942afc8a2a906d07/services/db374d86d0ff4080941af84a57380e36/execute?api-version=2.0&details=true'
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+    req = urllib2.Request(url, body, headers) 
+
+    try:
+        response = urllib2.urlopen(req)
+
+        # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
+        # req = urllib.request.Request(url, body, headers) 
+        # response = urllib.request.urlopen(req)
+
+        result = response.read()
+        #returns data as [p(0),p(1),predicted class]
+        return json.loads(result)['Results']['output1']['value']['Values'][0][-3:]
+    except urllib2.HTTPError, error:
+        print("The request failed with status code: " + str(error.code))
+
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
+
+        print(json.loads(error.read()))                 
