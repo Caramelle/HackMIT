6f2f43884152133dc53ba9c66e624cb001aaa266
caramelle <andreea.cucu@gmail.com>
2016-09-18 08:30:13 +0100

    exporting average scores to csv

--- sentimentAnalisys.py
+++ sentimentAnalisys.py
@@ -1,4 +1,6 @@
 import json
+import csv
+from tweetscraper import *
 from watson_developer_cloud import ToneAnalyzerV3
 
 
@@ -7,4 +9,28 @@ tone_analyzer = ToneAnalyzerV3(
     password= "0TGSUHaFxcpf",
     version="2016-02-11")
 
-print(json.dumps(tone_analyzer.tone(text='I am very happy'), indent=2))
+results = json.dumps(tone_analyzer.tone(text=tweets), indent=2)
+data = json.loads(results)
+title = searchText;
+anger = data['document_tone']['tone_categories'][0]['tones'][0]['score']
+disgust = data['document_tone']['tone_categories'][0]['tones'][1]['score']
+fear = data['document_tone']['tone_categories'][0]['tones'][2]['score']
+joy = data['document_tone']['tone_categories'][0]['tones'][3]['score']
+sadness = data['document_tone']['tone_categories'][0]['tones'][4]['score']
+analytical = data['document_tone']['tone_categories'][1]['tones'][0]['score']
+confident = data['document_tone']['tone_categories'][1]['tones'][1]['score']
+tentative = data['document_tone']['tone_categories'][1]['tones'][2]['score']
+openess = data['document_tone']['tone_categories'][2]['tones'][0]['score']
+conscientiousness = data['document_tone']['tone_categories'][2]['tones'][1]['score']
+extraversion = data['document_tone']['tone_categories'][2]['tones'][2]['score']
+agreeableness = data['document_tone']['tone_categories'][2]['tones'][3]['score']
+emotionalRange = data['document_tone']['tone_categories'][2]['tones'][4]['score']
+
+output = [title, anger, disgust, fear, joy, sadness, analytical, confident, tentative, openess, conscientiousness, extraversion, agreeableness, emotionalRange]
+
+ofile  = open('scores.csv', "wb")
+writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+writer.writerow(output)
+
+ofile.close()
--- tweetscraper.py
+++ tweetscraper.py
@@ -2,20 +2,23 @@
 import tweepy
 import json
 
-auth = tweepy.OAuthHandler("qLUhXHEaJxt8hn6SUhrTlNYaj", "2oZPOvHAy0vAi9gwEBgNX4W0xgx4SUzIjabgBa2UsIh8ZtSqmO")
+auth = tweepy.OAuthHandler("SFp7wv96ak2R1UxtPrAg5pKwY", "0shnNX1DfrBgVNwHaCLQlSfWZAHAUrije1bSR3KXsS5ZmpHhe6")
 auth.set_access_token("455064135-Xq71sKiQQUqBP7fWDWfjSqczQz1kTJwUhcy5T5Fs", "rGt7lms4cXFfJ2n7K3psUOGgeAGQkeSScwgLgzm2qliFF")
 
 api = tweepy.API(auth)
 
-searchText = "titanic"
+searchText = "barbie"
 searchNumber = "10"
 movie_tweets = api.search(searchText, rpp=searchNumber)
+
+tweets = "";
 
 for tweet in movie_tweets:
 	if "http" in tweet.text:
 		ok = 1
 	else:
-		print tweet.text + '\n'
+		tweets += tweet.text
+
 
 
 
